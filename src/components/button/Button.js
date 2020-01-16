33e527dc45a5ac9f7476a79d081a81808454223b
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-01-15 22:30:49 -0300

    cores e mixims na base, botao iniciado, router instalado

---
+++ src/components/button/Button.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import "./Button.scss";
+
+const Button = props => {
+  const buttonClass = "btn" + (props.disabled ? " disabled" : "");
+  const bgColor = props.bgColor || "#D7362A";
+  const bgColorDisabled = props.bgColorDisabled || "#CCC";
+  const buttonStyles = {
+    backgroundColor: props.disabled ? bgColorDisabled : bgColor,
+    width: props.width || "auto",
+    height: props.height || "40px",
+    marginTop: props.marginTop || "10px",
+    marginBottom: props.marginBottom || "10px",
+    marginLeft: props.marginLeft || "40px",
+    marginRight: props.marginRight || "40px",
+    paddingTop: props.paddingTop || "0px",
+    paddingBottom: props.paddingBottom || "0px",
+    paddingLeft: props.paddingLeft || "40px",
+    paddingRight: props.paddingRight || "40px",
+    display: props.display || "flex",
+    border: "none",
+    cursor: "pointer",
+    borderRadius: props.borderRadius || "5px",
+    fontSize: props.fontSize || "1.5em"
+  };
+  const defaultFunc = () => {
+    return;
+  };
+  return (
+    <>
+      <div className="buttonContainer">
+        <button
+          type={props.type || "button"}
+          className={buttonClass}
+          style={buttonStyles}
+          onClick={
+            !props.isLoading && typeof props.onClick === "function"
+              ? props.onClick
+              : defaultFunc
+          }
+        >
+          {props.isLoading ? <div className="loading-box"></div> : props.title}
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Button;
