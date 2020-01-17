a6e49b46490371d7be2ddc8e17494e6298a6383b
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-01-16 22:35:38 -0300

    navegação entre páginas funcionando + Redux e Axios instalados

--- src/pages/game/Game.js
+++ src/pages/game/Game.js
@@ -1,35 +1,49 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 import "./Game.scss";
 import Card from "../../components/card/Card";
 import Button from "../../components/button/Button";
 
-const Game = () => {
-  return (
-    <>
-      <section className="gameArea">
-        <Card />
-        <Button
-          title="Flip Another Card"
-          width="300px"
-          height="45px"
-          marginTop="50px"
-          borderRadius="3px"
-          fontFamily="Avenir Next Condensed"
-          fontSize="1.5rem"
-        />
-        <Button
-          title="Exit"
-          width="300px"
-          height="45px"
-          marginTop="30px"
-          borderRadius="3px"
-          fontFamily="Avenir Next Condensed"
-          fontSize="1.5rem"
-        />
-      </section>
-    </>
-  );
-};
+class Game extends Component {
+  state = {
+    redirectToStart: false
+  };
+
+  goToStart = () => {
+    this.setState({ redirectToStart: true });
+  };
+
+  render() {
+    if (this.state.redirectToStart) return <Redirect to={"/"} />;
+
+    return (
+      <>
+        <section className="gameArea">
+          <Card />
+          <Button
+            title="Flip Another Card"
+            width="300px"
+            height="45px"
+            marginTop="50px"
+            borderRadius="3px"
+            fontFamily="Avenir Next Condensed"
+            fontSize="1.5rem"
+          />
+          <Button
+            title="Exit"
+            width="300px"
+            height="45px"
+            marginTop="30px"
+            borderRadius="3px"
+            fontFamily="Avenir Next Condensed"
+            fontSize="1.5rem"
+            onClick={this.goToStart}
+          />
+        </section>
+      </>
+    );
+  }
+}
 
 export default Game;
--- src/pages/start/Start.js
+++ src/pages/start/Start.js
@@ -1,26 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
 import logoStarWars from "../../assets/logo-star-wars.png";
 import "./Start.scss";
 import Button from "../../components/button/Button";
 
-const Start = () => {
-  return (
-    <>
-      <div className="startArea">
-        <img src={logoStarWars} alt="Star Wars game" className="startLogo" />
-        <Button
-          title="Start"
-          width="300px"
-          height="45px"
-          marginTop="50px"
-          borderRadius="3px"
-          fontFamily="Avenir Next Condensed"
-          fontSize="1.5rem"
-        />
-      </div>
-    </>
-  );
-};
+class Start extends Component {
+  state = {
+    redirectToGame: false
+  };
+
+  goToGame = () => {
+    this.setState({ redirectToGame: true });
+  };
+
+  render() {
+    if (this.state.redirectToGame) return <Redirect to={"/game"} />;
+
+    return (
+      <>
+        <section className="startArea">
+          <img src={logoStarWars} alt="Star Wars game" className="startLogo" />
+          <Button
+            title="Start"
+            width="300px"
+            height="45px"
+            marginTop="50px"
+            borderRadius="3px"
+            fontFamily="Avenir Next Condensed"
+            fontSize="1.5rem"
+            onClick={this.goToGame}
+          />
+        </section>
+      </>
+    );
+  }
+}
 
 export default Start;
