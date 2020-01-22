5a8fa55ab5e9eddf8ffb6e473baefebe2b6ff4b8
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-01-21 23:14:40 -0300

    Updates

---
+++ precache-manifest.58b05bb005b933bcccebe809797e9885.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1b74b9fdc50dea47d9597c22c48611",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "6e1a8cbcc86b9780857b",
+    "url": "/star-wars/static/css/main.de575e31.chunk.css"
+  },
+  {
+    "revision": "d6c3839d1b3f60e926b6",
+    "url": "/star-wars/static/js/2.5da5b508.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/star-wars/static/js/2.5da5b508.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e1a8cbcc86b9780857b",
+    "url": "/star-wars/static/js/main.fc0c48c2.chunk.js"
+  },
+  {
+    "revision": "bb19ec81ea61bb80c6e1",
+    "url": "/star-wars/static/js/runtime-main.73f5171a.js"
+  },
+  {
+    "revision": "b325b83e596d23b6ccf2950fe3ed60b6",
+    "url": "/star-wars/static/media/bg-star.b325b83e.png"
+  },
+  {
+    "revision": "a8f978b8c5cee8d1561e2a6ccfc8077c",
+    "url": "/star-wars/static/media/card-planeta-frente.a8f978b8.png"
+  },
+  {
+    "revision": "c1b068ab83c2635bc73ca35710a6586d",
+    "url": "/star-wars/static/media/logo-star-wars.c1b068ab.png"
+  }
+]);
